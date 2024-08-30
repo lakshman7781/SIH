470a3061b30e7bc9747fbda5647296333605ed28
lakshman_reddy <147997328+lakshman7781@users.noreply.github.com>
2024-08-31 00:09:26 +0530

    chore: Add groq integration script and update requirements

---
+++ app/integrations/groq.py
@@ -0,0 +1,22 @@
+import os
+from groq import Groq
+
+api_key=os.environ.get["GROQ_KEY"]
+
+client = Groq(
+    api_key=api_key,
+)
+
+def get_chat_completion(prompt:str):
+
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+        model="llama-3.1-8b-instant",
+    )
+
+    return chat_completion.choices[0].message.content
