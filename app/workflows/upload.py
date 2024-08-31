77e2a9f6ad3f402ecdf1e2b76a88c85dc1c448cf
lakshman_reddy <147997328+lakshman7781@users.noreply.github.com>
2024-08-31 22:05:10 +0530

    chore: Refactor code to improve readability and maintainability

--- app/workflows/upload.py
+++ app/workflows/upload.py
@@ -4,4 +4,5 @@ from app.workflows.activities.upload import extract_text_from_pdf
 
 @flow(task_runner=SequentialTaskRunner())
 async def data_pipeline(file_path: str):
-    return extract_text_from_pdf(file_path)
+    text = extract_text_from_pdf(file_path)
+    return text
