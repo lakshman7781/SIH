9eb62dfc3a1dcfa74e0e12d3477152c8e32dddf7
lakshman_reddy <147997328+lakshman7781@users.noreply.github.com>
2024-09-28 21:30:53 +0530

    Update default port in main.py

--- main.py
+++ main.py
@@ -35,5 +35,5 @@ if APP_ENVIRONMENT == APP_ENVIRONMENT:
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 9292))
     uvicorn.run(app, host="0.0.0.0", port=port)
