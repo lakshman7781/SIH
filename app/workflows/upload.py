e36ce30d6dafca91cec085109165d013e884d8b4
lakshman_reddy <147997328+lakshman7781@users.noreply.github.com>
2024-09-29 22:44:51 +0530

    Refactor document_process_workflow function

--- app/workflows/upload.py
+++ app/workflows/upload.py
@@ -10,28 +10,32 @@ from app.constants import document_analysis_prompt
 from enum import Enum
 import textwrap
 from typing import List, Dict
+import logging
 
-
-
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def chunk_text(text: str, chunk_size: int = 1000, overlap: int = 100) -> List[str]:
     chunks = textwrap.wrap(text, chunk_size, break_long_words=False)
     return [chunks[0]] + [chunks[i-1][-overlap:] + chunks[i] for i in range(1, len(chunks))]
 
-
 @flow(task_runner=SequentialTaskRunner())
 async def document_process_workflow(file_path: str, document_type: DocumentType):
-    text = extract_text_from_pdf(file_path)
-    function_call = get_function_call(document_type)
-    if function_call is not None:
-        structured_text = extract_structured_data(input_text=text, function_list=function_call)
-        chunks = chunk_text(str(structured_text))
-        embeddings = [get_embeddings(chunk) for chunk in chunks] 
-        res= add_embedding_to_collection(file_path=file_path, chunks=chunks, embeddings=embeddings, document_type=DocumentType(document_type).name)
-        print(res)
-        
-    else:
-        # TODO: implement a long prompt to extract important information from the document that is dependent on the AI
-        structured_text=get_chat_completion_openai(prompt=document_analysis_prompt, text=text)
+    try:
+        text = extract_text_from_pdf(file_path)
+        function_call = get_function_call(document_type)
+        if function_call is not None:
+            structured_text = extract_structured_data(input_text=text, function_list=function_call)
+            chunks = chunk_text(str(structured_text))
+            embeddings = [get_embeddings(chunk) for chunk in chunks] 
+            res = add_embedding_to_collection(file_path=file_path, chunks=chunks, embeddings=embeddings, document_type=DocumentType(document_type).name)
+            logger.info(res)
+        else:
+            # TODO: implement a long prompt to extract important information from the document that is dependent on the AI
+            structured_text = get_chat_completion_openai(prompt=document_analysis_prompt, text=text)
+            return structured_text
         return structured_text
-    return structured_text
+    except Exception as e:
+        logger.error(f"Error in processing document: {str(e)}")
+        raise
